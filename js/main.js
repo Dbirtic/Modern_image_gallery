2897aadc1f4e7173c2dacf2d854de1c334516112
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-14 23:50:29 +0200

    added js code for switching images

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-// Write your code here...
+const current = document.querySelector('#current');
+const img = document.querySelectorAll('.imgs img');
+
+
+// looping through the images and looking for an click event to show the image
+img.forEach(img =>img.addEventListener('click', (e) => {current.src = e.target.src}));
